93d11c8509a2ebfa0660ccb81b337fb81f87d708
poojapatil <156627026+patil-poooja@users.noreply.github.com>
2024-11-25 11:32:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+        RWOnly ob = new RWOnly();
+        //'name' has private access in 'com.driver.RWOnly'
+        //ob.name="Pooja";
+        //System.out.println(ob.name);
+        ob.setName("Pooja");
+        System.out.println(ob.getName());
 }
